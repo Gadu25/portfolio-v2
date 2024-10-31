71ee3f810bb9c621a42dc1b94f1e35c5b0b03dca
Alexanderr Udag <alexanderudag@gmail.com>
2024-10-31 09:02:39 +0800

    updated my job roles, on different companies

--- components/card/swipingCards.vue
+++ components/card/swipingCards.vue
@@ -6,16 +6,16 @@
                     <div class="swiper-left hover-pointer" @click="handlePrev()">
                         <i class="fa fa-chevron-left"></i>
                     </div>
-                    <div class="swiper-right hover-pointer"@click="handleNext()">
+                    <div class="swiper-right hover-pointer" @click="handleNext()">
                         <i class="fa fa-chevron-right"></i>
                     </div>
                 </div>
             </div>
             <template v-for="(card, index) in cards">
-                <div class="card-wrapper" :class="card.position" @mousemove="(event) => handleMouseMove(event, card.position)"
+                <div class="card-wrapper" :class="card.position"
+                    @mousemove="(event) => handleMouseMove(event, card.position)"
                     @mouseleave="(event) => resetTilt(event, card.position)">
-                    <div class="card hover-pointer" @click="clickCard(index)"
-                        >
+                    <div class="card hover-pointer" @click="clickCard(index)">
                         <div class="company-wrapper">
                             <div class="company-title">
                                 <div class="company-logo">
@@ -27,7 +27,8 @@
                             </div>
                             <div class="my-role">
                                 <h4>{{ card.jobTitle }}</h4>
-                                <small>{{ card.date }}  <i v-if="card.duration.length > 0">. {{ card.duration }}</i></small>
+                                <small>{{ card.date }} <i v-if="card.duration.length > 0">. {{ card.duration
+                                        }}</i></small>
                             </div>
                             <div class="role-tasks">
                                 <ul>
@@ -61,10 +62,11 @@ export default {
                     date: '09/2024 - present',
                     duration: '',
                     roles: [
-                        'Developed a microservices architecture using Docker and Kubernetes, improving system scalability and reducing downtime by 30%.',
-                        'Designed and implemented RESTful APIs and GraphQL endpoints, ensuring seamless integration between frontend and backend systems.',
-                        'Optimized SQL queries and database schemas, resulting in a 40% performance boost across critical application processes.',
-                        'Automated CI/CD pipelines with GitHub Actions, minimizing deployment time and increasing development efficiency.'
+                        'Developed a comprehensive frontend component library using Vue.js and Nuxt to support a scalable and modular system architecture.',
+                        'Utilized Storybook to document and showcase reusable components, ensuring consistent UI/UX across the application.',
+                        'Crafted maintainable and responsive styles with SCSS, enhancing the design and adaptability of the frontend library.',
+                        'Implemented end-to-end testing with Cypress to ensure reliability and seamless functionality across components.',
+                        'Collaborated with cross-functional teams to integrate the frontend library effectively, enhancing development efficiency and consistency.'
                     ],
                     position: 'left'
                 },
@@ -75,10 +77,12 @@ export default {
                     date: '03/2023 - 08/2024',
                     duration: '(1 year 8 months)',
                     roles: [
-                        'Developed a microservices architecture using Docker and Kubernetes, improving system scalability and reducing downtime by 30%.',
-                        'Designed and implemented RESTful APIs and GraphQL endpoints, ensuring seamless integration between frontend and backend systems.',
-                        'Optimized SQL queries and database schemas, resulting in a 40% performance boost across critical application processes.',
-                        'Automated CI/CD pipelines with GitHub Actions, minimizing deployment time and increasing development efficiency.'
+                        'Developed and maintained a comprehensive Project Management Information System (PMIS) using Vue.js, Bootstrap, and the Laravel framework.',
+                        'Designed and implemented responsive layouts and UI components to enhance user experience across devices.',
+                        'Created and managed RESTful APIs to support data integration and communication between frontend and backend systems.',
+                        'Handled database management and optimization with MySQL, ensuring data integrity and efficient access across the system.',
+                        'Conducted thorough testing and debugging to ensure reliability and performance of the PMIS application.',
+                        'Delivered project presentations and demonstrations to stakeholders, showcasing system features and addressing feedback.'
                     ],
                     position: 'active'
                 },
@@ -89,10 +93,12 @@ export default {
                     date: '07/2022 - 02/2023',
                     duration: '(7 months)',
                     roles: [
-                        'Developed a microservices architecture using Docker and Kubernetes, improving system scalability and reducing downtime by 30%.',
-                        'Designed and implemented RESTful APIs and GraphQL endpoints, ensuring seamless integration between frontend and backend systems.',
-                        'Optimized SQL queries and database schemas, resulting in a 40% performance boost across critical application processes.',
-                        'Automated CI/CD pipelines with GitHub Actions, minimizing deployment time and increasing development efficiency.'
+                        'Designed and implemented responsive web pages using React, Next.js, Tailwind, and Bootstrap, ensuring cross-device compatibility and optimal user experience.',
+                        'Developed client-side logic in JavaScript, enhancing interactivity and functionality on web applications.',
+                        'Collaborated with the design team to translate UI/UX requirements into high-quality, user-friendly interfaces.',
+                        'Optimized page load performance and improved SEO by applying Next.js optimizations and best practices.',
+                        'Handled backend tasks, including managing databases with MySQL and creating RESTful APIs using the Laravel framework.',
+                        'Streamlined front-to-back communication by integrating frontend components with backend APIs, enhancing data flow and usability.'
                     ],
                     position: 'right'
                 }
@@ -117,11 +123,11 @@ export default {
                 }
             }
         },
-        handleNext(){
+        handleNext() {
             let activeIndex = 0
             for (let i = 0; i < this.cards.length; i++) {
                 if (this.cards[i].position == 'active') {
-                    activeIndex = i >= (this.cards.length - 1) ? 0 : i+1
+                    activeIndex = i >= (this.cards.length - 1) ? 0 : i + 1
                 }
             }
 
@@ -141,11 +147,11 @@ export default {
                 }
             }
         },
-        handlePrev(){
+        handlePrev() {
             let activeIndex = 0
             for (let i = 0; i < this.cards.length; i++) {
                 if (this.cards[i].position == 'active') {
-                    activeIndex = i <=  0 ? (this.cards.length - 1) : i-1
+                    activeIndex = i <= 0 ? (this.cards.length - 1) : i - 1
                 }
             }
 
