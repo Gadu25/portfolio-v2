fbfb4ae3a8e3dd65be6b30f951d5952665e27a6a
Gadu25 <alexanderudag@gmail.com>
2025-04-24 14:25:46 +0800

    added calc on some scss style and added 404 image

--- error.vue
+++ error.vue
@@ -1,10 +1,13 @@
 <template>
   <div class="error-wrapper">
+    <div class="display-image">
+      <img src="~/assets/images/memoji/404.webp" alt="Error Image" class="error-image" />
+    </div>
     <div class="text-center">
-      <h1 class="head-message">Oops!</h1>
-      <p class="error-message">{{ error.statusCode }} - {{ error.message }}</p>
+      <h1 class="head-message">{{ error.statusCode }}</h1>
+      <p class="error-message">{{ error.message }}</p>
       <NuxtLink to="/" class="back">
-        <SpecialButton buttonText="Back to home"/>
+        <SpecialButton buttonText="Go back"/>
       </NuxtLink>
     </div>
   </div>
