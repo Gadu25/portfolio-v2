972303ab15f2890b3f428ad368483b3a991321ef
Gadu25 <alexanderudag@gmail.com>
2026-07-30 11:28:03 +0800

    feat: Space Grotesk font and Swiss type scale

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -24,9 +24,9 @@ export default defineNuxtConfig({
           rel: 'stylesheet',
           href: 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css',
         },
-        { 
-          rel: 'stylesheet', 
-          href: 'https://fonts.googleapis.com/css2?family=Inter:wght@300;400;700&display=swap' 
+        {
+          rel: 'stylesheet',
+          href: 'https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@300;400;500;600;700&display=swap'
         },
       ],
       script: [
