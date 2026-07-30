f12bf7893d6ec6a775e0d2be1c65c8137c0eaef3
Gadu25 <alexanderudag@gmail.com>
2026-07-30 12:38:32 +0800

    fix: 404 page uses SpecialButton, remove dead regularworkexp file

--- pages/404.vue
+++ pages/404.vue
@@ -2,6 +2,12 @@
   <div class="error-wrapper">
     <h1 class="head-message">404</h1>
     <p class="error-message">Page not found</p>
-    <NuxtLink to="/" class="back">Go home</NuxtLink>
+    <NuxtLink to="/" class="back">
+      <SpecialButton button-text="Go home" />
+    </NuxtLink>
   </div>
 </template>
+
+<script setup lang="ts">
+import SpecialButton from '~/components/button/SpecialButton.vue'
+</script>
