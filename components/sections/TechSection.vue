e30867c335a07c048146937970d1e681be24e992
Gadu25 <alexanderudag@gmail.com>
2026-08-07 12:58:44 +0800

    refactor: replace tech icon grid with pure typography list — Swiss-consistent

--- components/sections/TechSection.vue
+++ components/sections/TechSection.vue
@@ -3,24 +3,13 @@
     <div class="tech__container">
       <h3>Technologies</h3>
       <div class="tech__grid">
-        <div
+        <span
           v-for="item in techs"
           :key="item.name"
           class="tech__item"
-          data-aos="fade-up"
         >
-          <a
-            class="tech__link hover-pointer"
-            :href="item.link"
-            target="_blank"
-            :aria-label="`Visit ${item.name} website`"
-          >
-            <img :src="item.icon" :alt="`${item.name}-icon`" />
-            <span class="tech__label">
-              <small>{{ item.name }}</small>
-            </span>
-          </a>
-        </div>
+          <small>{{ item.name }}</small>
+        </span>
       </div>
     </div>
   </section>
@@ -29,5 +18,5 @@
 <script setup lang="ts">
 import techstacks from '~/data/techs'
 
-const techs = ref(techstacks)
+const techs = techstacks
 </script>
