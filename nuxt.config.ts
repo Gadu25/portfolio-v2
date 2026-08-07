ae38ce5433b95e7e56022f239c48376735390cc3
Gadu25 <alexanderudag@gmail.com>
2026-08-07 13:34:21 +0800

    fix: disable nuxt devtools in production

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
-  devtools: { enabled: true },
+  devtools: { enabled: process.env.NODE_ENV !== 'production' },
   css: ['@/assets/css/main.scss'],
   app: {
     pageTransition: { name: 'page', mode: 'out-in' },
